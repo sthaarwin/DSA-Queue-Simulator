99ba7168708a5d1f418dd38d025b0107fcb55c6a
Axzyte <sthaarwin123@gmail.com>
2025-01-20 20:00:52 +0545

    initial setup

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <SDL.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    printf("SDL started successfully!\n");
+    SDL_Quit();
+    return 0;
+}
